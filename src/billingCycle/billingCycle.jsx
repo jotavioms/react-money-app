ef145b89cdf24c126db4efbeb938d339c93f1ee0
João Souza <j.otavioms@gmail.com>
2020-02-20 14:21:33 -0300

    Create tab header component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import Tabs from '../common/tab/tabs';
+import TabHeader from '../common/tab/tabHeader';
 import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 
@@ -14,10 +15,13 @@ class BillingCycle extends Component {
         <Content>
           <Tabs>
             <TabsHeader>
-
+              <TabHeader label='Listar' icon='bars' target='tabList' />
+              <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
             </TabsHeader>
             <TabsContent>
-              
+
             </TabsContent>
           </Tabs>
         </Content>
