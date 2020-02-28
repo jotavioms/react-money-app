87f3aac1a8e0ffb33e4e8f2b457e17d913bea802
João Souza <j.otavioms@gmail.com>
2020-02-28 09:44:00 -0300

    Create billing cycle form

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -10,7 +10,9 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabContent from '../common/tab/tabContent';
 import TabsContent from '../common/tab/tabsContent';
 import List from './billingCycleList';
+import Form from './billingCycleForm';
 import { selectTab, showTabs } from '../common/tab/tabActions';
+import { create } from './billingCycleActions';
 
 class BillingCycle extends Component {
   componentWillMount() {
@@ -34,7 +36,9 @@ class BillingCycle extends Component {
               <TabContent id='tabList'>
                 <List />
               </TabContent>
-              <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+              <TabContent id='tabCreate'>
+                <Form onSubmit={this.props.create} />
+              </TabContent>
               <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
               <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
             </TabsContent>
@@ -45,5 +49,5 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle);
