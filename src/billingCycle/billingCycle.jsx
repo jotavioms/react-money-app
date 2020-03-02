a37a5d3c05862f3ebec85902910b45d98161b928
João Souza <j.otavioms@gmail.com>
2020-03-02 11:05:32 -0300

    Create update method

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -12,7 +12,7 @@ import TabsContent from '../common/tab/tabsContent';
 import List from './billingCycleList';
 import Form from './billingCycleForm';
 import { selectTab, showTabs } from '../common/tab/tabActions';
-import { create } from './billingCycleActions';
+import { create, update } from './billingCycleActions';
 
 class BillingCycle extends Component {
   componentWillMount() {
@@ -40,7 +40,7 @@ class BillingCycle extends Component {
                 <Form onSubmit={this.props.create} />
               </TabContent>
               <TabContent id='tabUpdate'>
-                <Form />
+                <Form onSubmit={this.props.update} />
               </TabContent>
               <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
             </TabsContent>
@@ -51,5 +51,5 @@ class BillingCycle extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update }, dispatch);
 export default connect(null, mapDispatchToProps)(BillingCycle);
